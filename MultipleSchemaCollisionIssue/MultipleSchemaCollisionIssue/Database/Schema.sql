a9067d2699c38da767a195da9f6ec6274750487b
Michael Pendon <michael.c.pendon@outlook.com>
2020-06-18 06:03:59 +0200

    Added more updates.

---
+++ MultipleSchemaCollisionIssue/MultipleSchemaCollisionIssue/Database/Schema.sql
@@ -0,0 +1,12 @@
+﻿USE [TestDB]
+GO
+
+CREATE TABLE [survey].[Companies](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](256) NOT NULL
+) ON [PRIMARY]
+
+CREATE TABLE [directory].[Companies](
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](256) NOT NULL
+) ON [PRIMARY]
